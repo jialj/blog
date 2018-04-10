42b61bc1ceb5da56a0e823f82004097869c10efc
jialinjing <jialinjing@360.cn>
2018-04-10 22:50:11 +0800

    修改同步说明

--- publish/index.js
+++ publish/index.js
@@ -17,7 +17,7 @@ handler.on('error', function (err) {
 // 监听到push事件的时候执行我们的自动化脚本
 handler.on('push', function (event) {
   if(event.payload.ref === 'refs/heads/master'){
-    console.log('执行脚本，自动同步最新程序构建代码');
+    console.log('---自动同步最新程序,并完成构建!');
     runCommand('sh', ['./auto_build.sh'], function (txt) {
       console.log(txt)
     })
